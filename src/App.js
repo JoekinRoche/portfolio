27ce8e75e32cec9284c72d3520ddadaa6c607ebd
Joekin Roche <joekinroches@gmail.com>
2022-08-06 00:43:55 +0530

    About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
