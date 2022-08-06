b8668a4d92c14c6afddaebe37a312de7769b84fc
Joekin Roche <joekinroches@gmail.com>
2022-08-06 11:45:39 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Works from "./components/Works";
-import Contact from "./components/Contact";
-
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
+import Works from './components/Works';
 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Works />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Skills" element={<Skills />} />
+        <Route path="/Works" element={<Works />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
